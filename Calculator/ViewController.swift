43c9c10a018bd1b577cff91c978d34442a4d5e8b
Joechen <s900567@yahoo.com.tw>
2022-04-25 17:30:33 +0800

    [Modify] Refactor with Computed Properties(get/set)

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,12 +13,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     private var isFinishedTypingNumber: Bool = true
+
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to Double")
+            }
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
     
         isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            print(calcMethod)
+            
+            switch calcMethod {
+            case "AC":
+                displayValue = 0
+            case "+/-":
+                displayValue *= (-1)
+            case "%":
+                displayValue *= 0.01
+            case "÷":
+                print("÷")
+            case "×":
+                print("x")
+            case "-":
+                print("-")
+            case "+":
+                print("+")
+            case "=":
+                print("=")
+            default:
+                print(LocalizedError.self)
+            }
+        }
         
     }
 
@@ -33,7 +71,16 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
-                displayLabel.text = displayLabel.text! + numValue
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayValue += Double(numValue)!
             }
         }
     }
